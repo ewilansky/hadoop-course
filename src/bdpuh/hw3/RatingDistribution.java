e44a6a5c2fc9e549ef3d6825ba9fe7e6b532d939
Ethan Wilansky <ewilansky@yahoo.com>
2017-10-12 22:18:55 -0400

    Working on adding job config

--- src/bdpuh/hw3/RatingDistribution.java
+++ src/bdpuh/hw3/RatingDistribution.java
@@ -4,10 +4,32 @@
  */
 package bdpuh.hw3;
 
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+
+// import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapred.FileInputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+
 /**
  *
  * @author hdadmin
  */
 public class RatingDistribution {
+    
+    public static void main(String args[]) throws IOException {
+        Job ratingCountJob = null;
+        
+        ratingCountJob = Job.getInstance(new Configuration(), "RatingCount");
+        
+        // HDFS input path
+        TextInputFormat.addInputPath(ratingCountJob, new Path(args[0]));
+                
+        // input data format
+        ratingCountJob.setInputFormatClass(TextInputFormat.class);
+    
+    }
     
 }
